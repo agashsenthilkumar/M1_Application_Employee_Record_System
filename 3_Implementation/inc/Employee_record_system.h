8661aaefa8150433268075a47401f04b10cd9f76
agashsenthilkumar <94293980+agashsenthilkumar@users.noreply.github.com>
2021-11-24 23:29:52 +0530

    Update Employee_record_system.h

--- 3_Implementation/inc/Employee_record_system.h
+++ 3_Implementation/inc/Employee_record_system.h
@@ -1,1 +1,32 @@
+/** 
+* @file employee_record_system.h
+* exit or modity the record by 4 option
+*
+*/
 
+#ifndef __EMPLOYEE_RECORD_OPERATIONS_H__
+#define __EMPLOYEE_RECORD_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* function to set the
+*  coordinates
+**/
+void gotoxy(int x, int y)
+  
+/* Function to add the records */
+void addrecord()
+  
+/* Function to delete the records */
+void deleterecord()
+ 
+  /* Function to display the records */
+  void displayrecord()
+
+  /* Function to modify the records */
+  void modifyrecord()
+
+  #end if  /* #define __EMPLOYEE_RECORD_OPERATIONS_H__ */
